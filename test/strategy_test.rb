68b06dbce9ea3f9ece17ae3cc80c9f1fb4995285
Peter P <pete@p373.net>
2016-06-18 02:18:09 -0700

    fix test

--- test/strategy_test.rb
+++ test/strategy_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UidTest < StrategyTestCase
   def setup
     super
-    strategy.stubs(:raw_info).returns({ 'objectId' => '123' })
+    strategy.stubs(:raw_info).returns({ 'id' => '123' })
   end
 
   def test_return_id_from_raw_info

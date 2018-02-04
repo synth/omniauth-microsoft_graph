072c6e9aa0631ffd85588d956f9bd7a3e91e492c
Ryan Tolboom <Ryan.Tolboom@monroe.k12.nj.us>
2018-02-03 21:25:16 -0500

    Fixed callback_url issue

--- lib/omniauth/strategies/microsoft_graph.rb
+++ lib/omniauth/strategies/microsoft_graph.rb
@@ -38,6 +38,10 @@ module OmniAuth
         }
       end
 
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
       def raw_info
         @raw_info ||= access_token.get(authorize_params.resource + 'v1.0/me').parsed
       end
